2ebf6b4421e8a9b2370a6ebf5f9f5424a89b587a
miggow_16 <0306201552@caothang.edu.vn>
2023-06-08 23:11:03 +0700

    add migration

--- database/migrations/2023_06_08_160919_create_class_rooms_table.php
+++ database/migrations/2023_06_08_160919_create_class_rooms_table.php
@@ -15,6 +15,7 @@ class CreateClassRoomsTable extends Migration
     {
         Schema::create('class_rooms', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
             $table->timestamps();
         });
     }
